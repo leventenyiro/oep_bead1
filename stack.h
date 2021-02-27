60f75460fe9d3af86ca992d0c89e328676f39040
leventenyiro <nyiro.levente@gmail.com>
2021-02-27 21:53:44 +0100

    add links & new methods

--- stack.h
+++ stack.h
@@ -1,9 +1,18 @@
 #pragma once
 
+#define SIZE 10;
+
 class Stack {
     private:
         int *arr;
         int top;
     public:
+        stack(int size);
+        void push(int x) {
+            if (isFull()) {
+                
+            }
+        }
+    
         isEmpty()
 }
